c0eb4de0b6c425767647100b3ecce010bd413f59
vitor <vitormwxm@gmail.com>
2025-10-22 20:00:35 -0300

    Criando uma classe com três atributos para representar melhor o triângulo

--- src/Application/program.java
+++ src/Application/program.java
@@ -1,5 +1,7 @@
 package Application;
 
+import entities.Triangule;
+
 import java.sql.SQLOutput;
 import java.util.Locale;
 import java.util.Scanner;
@@ -9,23 +11,25 @@ public class program {
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
 
-        double xA , xB , xC, yA, yB, yC;
+        Triangule x, y;
+        x = new Triangule();
+        y = new Triangule();
 
         System.out.println("Enter de measures of triangule X: ");
-        xA = scanner.nextDouble();
-        xB = scanner.nextDouble();
-        xC = scanner.nextDouble();
+        x.a = scanner.nextDouble();
+        x.b = scanner.nextDouble();
+        x.c = scanner.nextDouble();
 
         System.out.println("Enter the measures of triangule Y");
-        yA = scanner.nextDouble();
-        yB = scanner.nextDouble();
-        yC = scanner.nextDouble();
+        y.a = scanner.nextDouble();
+        y.b = scanner.nextDouble();
+        y.c = scanner.nextDouble();
 
-        double p = (xA + xB + xC) / 2;
-        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+        double p = (x.a + x.b + x.c) / 2;
+        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
 
-        p = (yA + yB + yC) / 2;
-        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+        p = (y.a + y.b + y.c) / 2;
+        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
 
         System.out.printf("Triangle X area: %.4f%n", areaX);
         System.out.printf("Triangle Y area: %.4f%n", areaY);
