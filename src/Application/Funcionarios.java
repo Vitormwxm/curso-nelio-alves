6f7e23a16e360cfdcdb36b2d1f6f6b6a0b4956f1
vitor <vitormwxm@gmail.com>
2025-11-05 22:27:29 -0300

    resolvendo probleminha com vetor

--- src/Application/Funcionarios.java
+++ src/Application/Funcionarios.java
@@ -9,6 +9,21 @@ public class Funcionarios {
 
         Scanner scanner = new Scanner(System.in);
 
+        int n = scanner.nextInt();
+        double[] vect = new double[n];
 
+        for (int i = 0; i < n; i++) {
+            vect[i] = scanner.nextDouble();
+        }
+
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            sum += vect[i];
+        }
+
+        double avg = sum / n;
+        System.out.printf("AVERAGE HEIGHT: %.2f%n", + avg);
+
+        scanner.close();
     }
 }
