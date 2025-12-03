40728787c527a497ed4c660ff7e8b1d8c81ee653
vitor <vitormwxm@gmail.com>
2025-12-02 21:32:07 -0300

    Calculando Vespera de Natal

--- src/Application/CalculaVesperaDeNatal.java
+++ src/Application/CalculaVesperaDeNatal.java
@@ -1,9 +1,7 @@
 package Application;
 
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 public class CalculaVesperaDeNatal {
@@ -16,5 +14,13 @@ public class CalculaVesperaDeNatal {
         long diasFaltando = ChronoUnit.DAYS.between(dataAtual,vesperaNatal);
         System.out.println("Solução com ChronoUnit = " + diasFaltando);
         System.out.println("o Natal é em = " + diasParaNatal.toDays() + " dias");
+
+        DateTimeFormatter dataFormatada = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter horaFormatada = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        System.out.println(dataAtual.format(dataFormatada));
+
+        LocalTime localTime = LocalTime.now(ZoneId.systemDefault());
+        System.out.println(localTime.format(horaFormatada));
     }
 }
