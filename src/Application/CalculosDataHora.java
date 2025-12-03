5ae16e9aa7f391dacf88e42e78baaba355d8c96c
vitor <vitormwxm@gmail.com>
2025-12-02 21:31:29 -0300

    calculando data hora

--- src/Application/CalculosDataHora.java
+++ src/Application/CalculosDataHora.java
@@ -1,4 +1,30 @@
 package Application;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 public class CalculosDataHora {
+    public static void main(String[] args) {
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T16:20:30");
+        Instant d06 = Instant.parse("2022-07-20T01:10:33Z");
+
+        LocalDate pastWeekLocalDate = d04.minusDays(7);
+        LocalDate nextWeekLocalDate = d04.plusDays(7);
+
+        System.out.println("pastweekLocalDate = " + pastWeekLocalDate);
+        System.out.println("nextWeekLocalDate = " + nextWeekLocalDate);
+
+        Instant nextWeekInstant = d06.plus(7, ChronoUnit.DAYS);
+        System.out.println("nextWeekInstant = " + nextWeekInstant);
+
+        LocalDateTime pastWeekLocalDateTime = d05.minusDays(7);
+
+        Duration t1 = Duration.between(pastWeekLocalDateTime, d05);
+        System.out.println(t1.toHours());
+
+    }
 }
