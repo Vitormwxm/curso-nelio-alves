54f32311ece047df6d7b83648f13228328f19701
vitor <vitormwxm@gmail.com>
2025-12-21 22:32:25 -0300

    Upcast e downcast

--- src/Application/Application.java
+++ src/Application/Application.java
@@ -1,7 +1,35 @@
 package Application;
 
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        Account account = new Account();
+
+        Account acc1 = account;
+        Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+        Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
+
+        // Downcasting
+        BusinessAccount acc4 = (BusinessAccount) acc2;
+        acc4.loan(100.0);
+
+        if (acc3 instanceof BusinessAccount) {
+            BusinessAccount acc5 = (BusinessAccount) acc3;
+            acc5.loan(200.0);
+            System.out.println("Loan!!");
+        }
+
+        if (acc3 instanceof SavingsAccount) {
+            SavingsAccount acc5 = (SavingsAccount) acc3;
+            acc5.updateBalance();
+            System.out.println("Update");
+        }
     }
 }
