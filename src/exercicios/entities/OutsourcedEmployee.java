ad3a06ba50173d3d621d7421ed91f52e10d4ed1e
vitor <vitormwxm@gmail.com>
2025-12-22 22:32:53 -0300

    treinando polimorfismo com exercicioso

---
+++ src/exercicios/entities/OutsourcedEmployee.java
@@ -0,0 +1,35 @@
+package exercicios.entities;
+
+public class OutsourcedEmployee extends Employee{
+    private Double additionalCharge;
+
+    public OutsourcedEmployee() {
+    }
+
+    public OutsourcedEmployee(Double additionalCharge) {
+        this.additionalCharge = additionalCharge;
+    }
+
+    public OutsourcedEmployee(String name, Integer hours, Double valuePerHours, Double additionalCharge) {
+        super(name, hours, valuePerHours);
+        this.additionalCharge = additionalCharge;
+    }
+
+    public Double getAdditionalCharge() {
+        return additionalCharge;
+    }
+
+    public void setAdditionalCharge(Double additionalCharge) {
+        this.additionalCharge = additionalCharge;
+    }
+
+    @Override
+    public Double payment() {
+        return super.payment() + (additionalCharge * 110 / 100);
+    }
+
+    @Override
+    public String toString() {
+        return name + " - $" + payment() ;
+    }
+}
