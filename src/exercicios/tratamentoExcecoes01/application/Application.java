9ea8471ce82ade31cc78cdbc8384640e331a8308
vitor <vitormwxm@gmail.com>
2025-12-25 21:33:09 -0300

    Trantando acessoes do tipo RuntimeException

---
+++ src/exercicios/tratamentoExcecoes01/application/Application.java
@@ -0,0 +1,24 @@
+package exercicios.tratamentoExcecoes01.application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            String[] vect = scanner.nextLine().split(" ");
+            int position = scanner.nextInt();
+            System.out.println("Name: " + vect[position]);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Acessou uma posição inexistente no array");
+        } catch (InputMismatchException e) {
+            System.out.println("O valor digitado foi diferente de inteiro");
+        }
+
+        System.out.println("Fim do programa");
+
+    }
+}
