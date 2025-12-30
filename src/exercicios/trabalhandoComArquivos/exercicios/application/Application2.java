51c98c050102a8086b708f7e80ea7a1e7fe6bfd3
vitor <vitormwxm@gmail.com>
2025-12-30 00:01:42 -0300

    lógica de escrever arquivos em um csv

--- src/exercicios/trabalhandoComArquivos/exercicios/application/Application2.java
+++ src/exercicios/trabalhandoComArquivos/exercicios/application/Application2.java
@@ -4,11 +4,13 @@ import exercicios.trabalhandoComArquivos.exercicios.entities.Product;
 
 import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.io.*;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Application2 {
     public static void main(String[] args) {
-
+        Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite o caminho do arquivo que deseja fazer a leitura");
         String strPath = scanner.nextLine();
@@ -43,11 +45,21 @@ public class Application2 {
         String fileName = scanner.nextLine();
         File fileStrPath = new File(strPath);
         boolean file = new File(fileStrPath.getParent() + "/" + fileName).mkdir();
-        System.out.println(file);
-//        try (BufferedWriter wr = new BufferedWriter(new FileWriter(fileName))){
-//
-//        } catch (IOException e) {
-//
-//        }
+
+        File directory = new File(fileStrPath.getParent() + "/" + fileName);
+        File createFile = new File(directory, "summary.csv");
+
+        try (BufferedWriter wr = new BufferedWriter(new FileWriter(createFile))){
+
+
+            for (Product p : product.getList()) {
+                String line = p.getProduct() + "," + String.format("%.2f", p.totalValue());
+                wr.write(line);
+                wr.newLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
