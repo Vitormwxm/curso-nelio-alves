e898915470a34049c7eb6c5f335b414ae4af4115
vitor <vitormwxm@gmail.com>
2026-01-02 21:21:04 -0300

    criação da classe program

---
+++ src/exercicios/interfaces/exercicio/application/Program.java
@@ -0,0 +1,11 @@
+package exercicios.interfaces.exercicio.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+    }
+}
