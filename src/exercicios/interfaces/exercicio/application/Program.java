59bf94cd2735ba596aef744f75b8d76ff7c91f48
vitor <vitormwxm@gmail.com>
2026-01-02 21:31:53 -0300

    finalizando logica de associação da classe carModel com Vehicle

--- src/exercicios/interfaces/exercicio/application/Program.java
+++ src/exercicios/interfaces/exercicio/application/Program.java
@@ -1,5 +1,10 @@
 package exercicios.interfaces.exercicio.application;
 
+import exercicios.interfaces.exercicio.model.entities.CarRental;
+import exercicios.interfaces.exercicio.model.entities.Vehicle;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,5 +12,18 @@ public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        System.out.println("Entre com os dados do aluguel");
+        System.out.println("Modelo do carro: ");
+        String carModel = scanner.nextLine();
+        System.out.println("Retirada (dd/MM/yyyy hh:mm): ");
+        LocalDateTime start = LocalDateTime.parse(scanner.nextLine(), dateTimeFormatter);
+        System.out.println("Entrada (dd/MM/yyyy hh:mm): ");
+        LocalDateTime finish = LocalDateTime.parse(scanner.nextLine(), dateTimeFormatter);
+
+        CarRental carRental = new CarRental(start, finish, new Vehicle(carModel));
+
+
     }
 }
